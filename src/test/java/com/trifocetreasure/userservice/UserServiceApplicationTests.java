d687db2ebca2366b69f57c5acf8bfd29b18c9a01
Mateus Silva Freitas <mateus.freitas@paytrack.com.br>
2023-12-12 13:52:22 -0300

    Project structure

---
+++ src/test/java/com/trifocetreasure/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trifocetreasure.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
